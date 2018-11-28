dfc7ec1ee34a28b29beb504803f3712da7a12a5f
mrpqzm <45233889+mrpqzm@users.noreply.github.com>
2018-11-28 18:00:10 +0800

    Create DynamicInfiniteScroll.py

---
+++ DynamicInfiniteScroll.py
@@ -0,0 +1,11 @@
+loopCounter = 0
+lastHeight = browser.execute_script("return document.body.scrollHeight")
+while True:
+    if loopCounter > 499:
+        break # if the account follows a ton of people, its probably a bot, cut it off
+    browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    time.sleep(2)
+    newHeight = browser.execute_script("return document.body.scrollHeight")
+    if newHeight == lastHeight:
+        break
+    lastHeight = newHeight
